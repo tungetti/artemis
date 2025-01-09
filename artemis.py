183dea9d1aeb730e127df5936ba8bef21f9b1236
Tommaso Maria Ungetti <tommaso.ungetti@outlook.com>
2025-01-09 18:01:02 +0100

    refactoring and code cleaning

--- artemis.py
+++ artemis.py
@@ -27,6 +27,8 @@ def get_access_token(scope):
     print(f"[ERROR]: Failed to acquire token. {e}")
     return None
 
+# ENTRAID
+
 # Function to make Graph API calls
 def make_graph_call(url, scope, pagination=True):
   access_token = get_access_token(scope)
@@ -52,51 +54,6 @@ def make_graph_call(url, scope, pagination=True):
       break
 
   return graph_results
-
-# Function to make calls to Azure Management API
-def make_management_api_call(url, scope, pagination=True):
-  access_token = get_access_token(scope)
-  if not access_token:
-    print("[ERROR]: No access token available.")
-    return []
-
-  headers = {'Authorization': f'Bearer {access_token}'}
-  resources = []
-
-  while url:
-    try:
-      response = requests.get(url, headers=headers)
-      response.raise_for_status()
-      data = response.json()
-      resources.extend(data.get('value', []))
-      if pagination and 'nextLink' in data:
-        url = data['nextLink']
-      else:
-        break
-    except Exception as e:
-      print(f"[ERROR]: Failed to fetch data. {e}")
-      break
-
-  return resources
-
-# Workbook setup
-workbook = openpyxl.load_workbook('./source/template_entraid.xlsx')
-users_sheet = workbook['Users']
-groups_sheet = workbook['Groups']
-
-# Fetch and process users
-def fetch_subscriptions_v2():
-  url = "https://management.azure.com/subscriptions?api-version=2021-04-01"
-  scope = "https://management.azure.com/.default"
-  subscriptions_arm_api = make_management_api_call(url, scope)
-  return [
-    {
-      'subscriptionId': subscription_int['subscriptionId'],
-      'displayName': subscription_int['displayName'],
-      'state': subscription_int['state']
-    }
-    for subscription_int in subscriptions_arm_api
-  ]
 
 def fetch_users():
   url = 'https://graph.microsoft.com/v1.0/users'
@@ -139,11 +96,53 @@ def fetch_groups():
     for group in groups
   ]
 
+# AZURE RESOURCES
+
+# Function to make calls to Azure Management API
+def make_management_call(url, scope, pagination=True):
+  access_token = get_access_token(scope)
+  if not access_token:
+    print("[ERROR]: No access token available.")
+    return []
+
+  headers = {'Authorization': f'Bearer {access_token}'}
+  resources = []
+
+  while url:
+    try:
+      response = requests.get(url, headers=headers)
+      response.raise_for_status()
+      data = response.json()
+      resources.extend(data.get('value', []))
+      if pagination and 'nextLink' in data:
+        url = data['nextLink']
+      else:
+        break
+    except Exception as e:
+      print(f"[ERROR]: Failed to fetch data. {e}")
+      break
+
+  return resources
+
+# Fetch and process users
+def fetch_subscriptions_v2():
+  url = "https://management.azure.com/subscriptions?api-version=2021-04-01"
+  scope = "https://management.azure.com/.default"
+  subscriptions_arm_api = make_management_call(url, scope)
+  return [
+    {
+      'subscriptionId': subscription_int['subscriptionId'],
+      'displayName': subscription_int['displayName'],
+      'state': subscription_int['state']
+    }
+    for subscription_int in subscriptions_arm_api
+  ]
+
 # Fetch resources from ARM API
 def fetch_resources_v2(subscription):
   scope = "https://management.azure.com/.default"
   url = f"https://management.azure.com/subscriptions/{subscription}/resources?api-version=2021-04-01"
-  resources = make_management_api_call(url, scope)
+  resources = make_management_call(url, scope)
   return [
     [
       resource['id'],
@@ -161,6 +160,12 @@ def append_data_to_sheet(sheet, data):
 
 # Main logic
 if __name__ == "__main__":
+
+  # Workbook setup
+  workbook = openpyxl.load_workbook('./source/template_entraid.xlsx')
+  users_sheet = workbook['Users']
+  groups_sheet = workbook['Groups']
+
   # Process users
   users_data = fetch_users()
   append_data_to_sheet(users_sheet, users_data)
